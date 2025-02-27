144c5d5eba8db6f5995dfd04c223b3cabeff0af1
Arina Kuratova <153686750+Rina2003123@users.noreply.github.com>
2025-02-27 07:01:23 +0000

    1

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,7 +17,30 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
+from .models import Vladenie 
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Vladenie
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = Vladenie.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include(router.urls)),
+    path('api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
